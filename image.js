231477465928d49b77d5912e70c3d9174e2848fa
tcode2k16 <tcode2k16@users.noreply.github.com>
2016-09-24 21:34:08 +0800

    Add files via upload

---
+++ image.js
@@ -0,0 +1,18 @@
+/**
+ * Created by home on 1/10/15.
+ */
+
+var Wall = new Image;
+Wall.src = "wall.png";
+
+var Road = new Image;
+Road.src = "road.png";
+
+var Exit = new Image;
+Exit.src = "exit.png";
+
+var Monster = new Image;
+Monster.src = "monster.png";
+
+var Player = new Image;
+Player.src = "player.png";
